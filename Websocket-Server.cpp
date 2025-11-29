cd58d791751ec0598fa062c0ee609e1771bbc557
adkostatt <adkostatt@gmail.com>
2025-11-29 13:17:53 +0300

    Добавьте файлы проекта.

---
+++ Websocket-Server.cpp
@@ -0,0 +1,12 @@
+﻿// Websocket-Server.cpp: определяет точку входа для приложения.
+//
+
+#include "Websocket-Server.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
